9c4bbe2717eabe094c0917d3eb5b9ba8ab09bf75
Sumit Ghosh <ghosh.sumit1@gmail.com>
2024-08-18 09:55:29 +0530

    Added sequential & parallel run

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ module.exports.responseHooks = [
 
 module.exports.requestGroupActions = [
     {
-        label: "Run All Requests",
+        label: "Run All Requests-Parallel",
         action: async (context, data) => {
 
             if (data.requests.length == 0) {
@@ -170,12 +170,124 @@ module.exports.requestGroupActions = [
             }).catch((error) => {
                 let reqNo = error.message.match(/req_\w*/g)
                 console.log("Error in Promise.all", reqNo[0])
-                const html = `<ul>Error in request or reponse : ${map.get(reqNo[0])[0]}</ul>`;
+                const html = `<ul>Error in request host, headers or reponse : ${map.get(reqNo[0])[1]}</ul>`;
                 context.app.showGenericModalDialog('Test Run Results' + data.requestGroup.name, { html });
                 console.log("Error in Promise.all", error)
                 console.error(error.message);
             });
-
-        },
+        }
     },
+    {
+        label: "Run All Requests-Sequential",
+        action: async (context, data) => {
+
+            if (data.requests.length == 0) {
+                console.log("No requests to run")
+                const html = `<hr class="pad-top"><p class="notice info no-margin-top">Run on folder containing requestes. See <a href="https://github.com/gsumit1/insomnia-plugin-response-validator" class="theme--link">Documentation</a> for more info.</p>`;
+                context.app.showGenericModalDialog('No Requestes To Process For Folder : ' + data.requestGroup.name, { html });
+                return
+            }
+
+            const { requests } = data;
+            let results = [];
+            let map = new Map();
+            results.push(`<thead><tr><th>Request Name</><th>Result</th></tr></thead><tbody>`);
+
+            for (const request of requests) {
+                let temp = [];
+                temp.push(request.method)
+                temp.push(request.name);
+                try {
+                    let requestHeader = request.headers.filter(header => header.name === "INSOMNIA-RESPONSE-VALIDATOR");
+                    let jsonPath = requestHeader[0].value;
+                    temp.push(jsonPath);
+                } catch (error) { temp.push(false); }
+                map.set(request._id, temp)
+
+
+                try {
+                    const entireResponse = await context.network.sendRequest(request)
+                    try {
+                        reqDetails = map.get(entireResponse.parentId);
+                    } catch (error) { }
+                    let reqMethod = reqDetails[0];
+                    let reqName = reqDetails[1];
+                    let jsonPath = reqDetails[2];
+                    let errors = 0;
+
+
+                    if (jsonPath) {
+                        try {
+                            actualStatusCode = entireResponse.statusCode;
+
+                        } catch (error) {
+                            console.error("Error in the response status code", error);
+                            return;
+                        }
+
+                        try {
+                            jsonObject = JSON.parse(jsonPath.toString());
+                        } catch (error) {
+                            console.error("Error in INSOMNIA-RESPONSE-VALIDATOR header or in parsing the header", error);
+                            return
+                        }
+
+                        console.log("********** Test Execution Started From Bulk Request For " + reqName + " **************")
+                        Object.keys(jsonObject).forEach(key => {
+                            try {
+                                if (key == "INSA-ResponseCode") {
+                                    __.assertThat("Status Code validation failed. The expected status code " + jsonObject[key] + " but the actual status code " + actualStatusCode, actualStatusCode, __.is(parseInt(jsonObject[key])));
+                                } else {
+
+                                    if (entireResponse.bodyPath) {
+                                        const responseBody = JSON.parse(fs.readFileSync(entireResponse.bodyPath, 'utf8'));
+                                        response = JSON.parse(JSON.stringify(responseBody));
+                                        var actualValues = jp.query(response, key)
+                                        if (typeof jsonObject[key] === 'string' || jsonObject[key] instanceof String) {
+                                            __.assertThat(actualValues, eval(jsonObject[key]))
+                                            console.log("Test For JSONPATH :" + key + ": Passed");
+                                        }
+                                    } else {
+                                        errors = NaN
+                                    }
+                                }
+
+                            } catch (error) {
+                                errors++;
+                                console.error("Either JSONPATH or Key has issue in request " + reqName + "\n" + error);
+                            }
+                        })
+
+                        if (errors == 0) {
+                            results.push(`<tr><td>${reqMethod} : ${reqName}</td><td>Passed</td></tr>`);
+                            console.log("Assertion Passed");
+                        } else if (errors > 0) {
+                            results.push(`<tr><td>${reqMethod} : ${reqName}</td><td>Failed</td></tr>`);
+                            console.error("Assertion Failed");
+                        } else if (errors === NaN) {
+                            results.push(`<tr><td">${reqMethod} : ${reqName}</td><td>Timeout or error</td></tr>`);
+                            console.log("Assertion Unknown");
+                        }
+
+                    } else {
+                        if (entireResponse.statusCode > 0) { results.push(`<tr><td>${reqMethod} : ${reqName}</td><td>Status - ${entireResponse.statusCode} ${entireResponse.statusMessage}</td></tr>`) }
+                        else { results.push(`<tr><td>${reqMethod} : ${reqName}</td><td>Timeout or Error </td></tr>`) }
+                    }
+
+                } catch (error) {
+
+                    let reqNo = error.message.match(/req_\w*/g)
+                    console.log("Error in request", reqNo[0])
+                    console.error(error.message);
+                    results.push(`<tr><td>${map.get(reqNo[0])[0]} : ${map.get(reqNo[0])[1]}</td><td>Error in request, header or host</td></tr>`);
+                    console.log("Error in request");
+                }
+            }
+
+            results.push(`</tbody>`);
+
+            const html = `<a class="sc-himrzO gofaxW made-with-love" href="https://github.com/gsumit1/insomnia-plugin-response-validator/">See&nbsp;<u>Documentation</u>&nbsp;for more info </a><hr class="pad-top"><table class="table--fancy table--striped table--valign-middle margin-top margin-bottom">${results.join('\n')}</table><a class="sc-himrzO gofaxW made-with-love" href="https://github.com/gsumit1/insomnia-plugin-response-validator/">Made with&nbsp; <div class="sc-dIouRR bNAJOc"><svg viewBox="0 0 24 24" width="1em" height="1em" role="img"><path d="m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path></svg></div> &nbsp;by Sumit</a>`;
+            context.app.showGenericModalDialog('Test Execution Results Of Folder : ' + data.requestGroup.name, { html });
+        }
+    }
 ];
